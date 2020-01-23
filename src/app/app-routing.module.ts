9337349688b30ab43bc780980b29acbc993ac692
Aisha Blake <aisha.g.blake@gmail.com>
2020-01-22 19:10:35 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: '', redirectTo: '/page1', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
